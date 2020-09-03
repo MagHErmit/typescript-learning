33c0d45f010c8377b7002b4c7907a5f5b5c41a8e
MagHErmit <->
2020-09-04 02:33:52 +1000

    minor changes

--- First task/second.ts
+++ First task/second.ts
@@ -7,9 +7,9 @@ async function main() {
   return await axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
       function(response) {
         const list: string[] = []
-        for (const i in response.data) {
-          list.push(response.data[i].Name)
-        }
+        response.data.forEach((n) => {
+          list.push(n.Name)
+        })
         return list
       }
   )
--- Web-deploy/web-countries.ts
+++ Web-deploy/web-countries.ts
@@ -8,15 +8,21 @@ async function main() {
   return await axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
       function(response) {
         const list: string[] = []
-        response.data.forEach((n) => {
-          list.push(n.Name)
-        })
+        for (const i in response.data) {
+          list.push(response.data[i].Name)
+        }
         return list
       }
   )
 }
-main().then((countries) => {
-  console.log(countries)
-}).catch((err) => {
-  console.error(err)
-})
+
+// main().then((countries) => {
+//   return countries
+// }).catch((err) => {
+//   console.error(err)
+// })
+
+
+exports.helloGET = (req, res) => {
+  res.send('Hello World!')
+}
