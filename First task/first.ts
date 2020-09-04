60c316315cf00498f7ea78203cae96baf90c0284
MagHErmit <->
2020-09-04 12:05:32 +1000

    install xo, linting code after that

--- First task/first.ts
+++ First task/first.ts
@@ -1,22 +1,22 @@
-/* eslint-disable guard-for-in */
-export {} // fix for "Cannot redeclare block-scoped variable"
-const axios = require('axios').default
+export {}; // Fix for "Cannot redeclare block-scoped variable"
+const axios = require('axios').default;
 /**
  * JSDoc annotation
  */
 async function main() {
-  return await axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
-      function(response) {
-        const list: string[] = []
-        response.data.forEach((n) => {
-          list.push(n.Name)
-        })
-        return list
-      }
-  )
+  return axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
+    response => {
+      const list: string[] = [];
+      response.data.forEach(n => {
+        list.push(n.Name);
+      });
+      return list;
+    }
+  );
 }
-main().then((countries) => {
-  console.log(countries)
-}).catch((err) => {
-  console.error(err)
-})
+
+main().then(countries => {
+  console.log(countries);
+}).catch(err => {
+  console.error(err);
+});
--- First task/second.ts
+++ First task/second.ts
@@ -1,24 +1,24 @@
-/* eslint-disable guard-for-in */
-const axios = require('axios').default
+const axios = require('axios').default;
 /**
  * JSDoc annotation
  */
 async function main() {
-  return await axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
-      function(response) {
-        const list: string[] = []
-        response.data.forEach((n) => {
-          list.push(n.Name)
-        })
-        return list
-      }
-  )
+  return axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
+    response => {
+      const list: string[] = [];
+      response.data.forEach(n => {
+        list.push(n.Name);
+      });
+      return list;
+    }
+  );
 }
+
 (async () => {
   try {
-    const countries: string = await main()
-    console.log(countries)
+    const countries: string = await main();
+    console.log(countries);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-})()
+})();
