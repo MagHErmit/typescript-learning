73971578b585e0d490743ce64667620c3e47907f
MagHErmit <->
2020-09-06 13:58:22 +1000

    minor improvments

--- First task/first.ts
+++ First task/first.ts
@@ -1,8 +1,5 @@
-export {}; // Fix for "Cannot redeclare block-scoped variable"
-const axios = require('axios').default;
-/**
- * JSDoc annotation
- */
+import axios from 'axios';
+
 async function main() {
   return axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
     response => {
--- First task/second.ts
+++ First task/second.ts
@@ -1,7 +1,5 @@
-const axios = require('axios').default;
-/**
- * JSDoc annotation
- */
+import axios from 'axios';
+
 async function main() {
   return axios.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').then(
     response => {
@@ -16,7 +14,7 @@ async function main() {
 
 (async () => {
   try {
-    const countries: string = await main();
+    const countries: string[] = await main();
     console.log(countries);
   } catch (err) {
     console.error(err);
